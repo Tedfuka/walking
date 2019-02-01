e3cd042ab09be9b9911542c1d1b621fe7022062d
深澤哲也 <t-fuka@zk9.so-net.ne.jp>
2019-02-01 20:24:21 +0900

    flash

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -1,18 +1,40 @@
 class StepsController < ApplicationController
   def index
-    @teams = User.joins(:steps).group(:team).order('average_step DESC').average(:step)
-    @users = User.joins(:steps).group(:user_id).order('average_step DESC').average(:step)
+    steps2 = User.joins(:steps).where("date between '2019-02-01 00:00:00' and '2019-2-28 23:59:59'")
+    @teams = steps2.group(:team).order('average_step DESC').average(:step)
+    @users = steps2.group(:user_id).order('average_step DESC').average(:step)
     @i = 1
     @j = 1
   end
 
   def new
-    @steps = Step.where(user_id: current_user.id).order('date ASC')
     @step = Step.new
+    @steps = Step.where(user_id: current_user.id).where("date between '2019-02-01 00:00:00' and '2019-2-28 23:59:59'").order('date ASC')
   end
 
   def create
-    step = Step.create(step_params)
+    @step = Step.new(step_params)
+    @steps = Step.where(user_id: current_user.id).where("date between '2019-02-01 00:00:00' and '2019-2-28 23:59:59'").order('date ASC')
+    if @step.save
+    redirect_to new_step_path, notice: "登録完了しました"
+    else
+    redirect_to new_step_path, alert: "入力されていない項目があります"
+    end
+  end
+
+  def edit
+    @step = Step.find(params[:id])
+  end
+
+  def update
+    step = Step.find(params[:id])
+    step.update(step_params)
+    redirect_to new_step_path
+  end
+
+  def destroy
+    step = Step.find(params[:id])
+    step.destroy #予め定義されているdestroyメソッド
     redirect_to new_step_path
   end
 
--- app/models/step.rb
+++ app/models/step.rb
@@ -1,3 +1,5 @@
 class Step < ApplicationRecord
+  validates :step, presence: true
+  validates :date, presence: true
   belongs_to :user, optional: true
 end

d5970298c7fa9e0290096f05fe9bca686163b8f0
深澤哲也 <t-fuka@zk9.so-net.ne.jp>
2019-02-02 17:06:55 +0900

    ver2

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -38,6 +38,14 @@ class StepsController < ApplicationController
     redirect_to new_step_path
   end
 
+  def show
+    steps2 = User.joins(:steps).where("date between '2019-02-01 00:00:00' and '2019-2-28 23:59:59'")
+    @teams = steps2.group(:team).order('average_step DESC').average(:step)
+    @users = steps2.group(:user_id).order('average_step DESC').average(:step)
+    @i = 1
+    @j = 1
+  end
+
   private
   def step_params
       params.require(:step).permit(:step, :date).merge(user_id: current_user.id)
